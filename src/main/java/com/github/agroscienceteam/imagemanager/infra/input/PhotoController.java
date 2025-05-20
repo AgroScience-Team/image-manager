33ce2d3aabaafbdfdcf56d055897801e4eb03629
Alexander Avershin <55056382+asavershin@users.noreply.github.com>
2025-05-20 21:50:15 +0300

    13 Rename url (#14)

--- src/main/java/com/github/agroscienceteam/imagemanager/infra/input/PhotoController.java
+++ src/main/java/com/github/agroscienceteam/imagemanager/infra/input/PhotoController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/image-manager")
+@RequestMapping("/api/v1/image-manager")
 public class PhotoController {
 
   private final PhotoRepository repo;
